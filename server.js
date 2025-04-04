17c4df1a5e2b164e8a12f0bd50f7d1c0db579101
harsha_tk <harsha.v@pycube.com>
2025-04-04 23:50:47 +0530

    test route

--- server.js
+++ server.js
@@ -9,5 +9,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api', userRoutes);
 
+app.get('/test', (req, res) => {
+    res.send('Hello World!');
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
